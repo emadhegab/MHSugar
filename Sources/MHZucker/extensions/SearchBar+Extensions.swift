013a8d3d2edeeb265b7543f918c16a6754b7d84a
Mohamed Hegab <mohamed.hegab@verimi.com>
2020-03-27 09:45:43 +0100

    add new search bar extension

---
+++ Sources/MHZucker/extensions/SearchBar+Extensions.swift
@@ -0,0 +1,25 @@
+//
+//  File.swift
+//  
+//
+//  Created by Mohamed Hegab on 27.03.20.
+//
+
+import UIKit
+
+extension UISearchBar {
+
+    var textField : UITextField? {
+        if #available(iOS 13.0, *) {
+            return self.searchTextField
+        } else {
+            // Fallback on earlier versions
+            for view: UIView in (self.subviews[0]).subviews {
+                if let textField = view as? UITextField {
+                    return textField
+                }
+            }
+        }
+        return nil
+    }
+}
